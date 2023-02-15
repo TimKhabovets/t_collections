269e66a8b48a2281baa834eb75831b015591d82d
Timosha <flykaiq449@gmail.com>
2023-02-15 16:20:28 +0300

    fix

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -19,8 +19,8 @@ export const login = async (req, res, next) => {
     const user = await loginUser(email, password);
     res.cookie('refreshToken', user.refreshToken, {
       maxAge: 30*24*60*60*1000, 
-      httpOnly: true,
-      domain: '.t-collections.vercel.app', 
+      httpOnly: false,
+      domain: 't-collections.vercel.app', 
       sameSite: "none",
       secure: true,
     });
@@ -53,7 +53,7 @@ export const refreshTokens = async (req, res, next) => {
     res.cookie('refreshToken', user.refreshToken, {
       maxAge: 30*24*60*60*1000, 
       httpOnly: true, 
-      domain: '.vercel.app', 
+      domain: 't-collections.vercel.app', 
       sameSite: "none",
       secure: true,
     });
@@ -72,7 +72,7 @@ export const createUser = async (req, res, next) => {
     res.cookie('refreshToken', user.refreshToken, { 
       maxAge: 30*24*60*60*1000, 
       httpOnly: true, 
-      domain: '.vercel.app', 
+      domain: 't-collections.vercel.app', 
       sameSite: "none",
       secure: true,
     });
